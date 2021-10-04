c2ecc165d45a3a771c591c972d3bb68e78ae3bd4
Olena Stoliarova <tajlintajlin@gmail.com>
2021-10-04 23:01:48 +0300

    add db contest and resul model

---
+++ GlaucomaWay/GlaucomaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using GlaucomaWay.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlaucomaWay
+{
+    public class GlaucomaDbContext : DbContext
+    {
+        public DbSet<Vf14ResultModel> Vf14Results { get; set; }
+
+        public GlaucomaDbContext(DbContextOptions<GlaucomaDbContext> options)
+         : base(options)
+        {
+        }
+
+        public GlaucomaDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
